1c36ae1aeb9a09a11db66eecc93baf461528ccd9
Faraaz Khan <faraaz@rationalizeit.us>
2012-03-05 19:27:21 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EresponseManager::Application.config.secret_token = '77cb788590750679719a0f1239c6d8fb6512224129a488c02e7d1fe4d63f98c8c56c65734357030520e364b55b0443eda20bfc43c0f88e2235baa7d12dccb709'
