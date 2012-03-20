1de99132a6adc19b916311be29f2f4a36d9222f9
Faraaz Khan <faraaz@rationalizeit.us>
2012-03-19 22:25:07 -0400

    can I use constants in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,16 @@ EresponseManager::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'rationalizeit.us',
+    :user_name            => GMAIL_USERNAME,
+    :password             => GMAIL_PWD,
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
